bb0be3ccbaeb47648bbc9634893aca66a56a9931
Ulpan Beken <u.beken@typi.team>
2023-01-07 02:51:24 +0600

    added functionality to add button

--- app/src/main/java/kz/jusan/singularityhomeworks/AddButtonViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package kz.jusan.singularityhomeworks
-
-import android.view.View
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
-
-class AddButtonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val btnAdd : Button = itemView.findViewById(R.id.btn_add)
-}
