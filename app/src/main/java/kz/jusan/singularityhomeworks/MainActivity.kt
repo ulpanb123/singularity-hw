1e2c3d7002ba7ad4b8805acb1b51704710538a97
Ulpan Beken <u.beken@typi.team>
2023-01-11 16:47:08 +0600

    init

--- app/src/main/java/kz/jusan/singularityhomeworks/MainActivity.kt
+++ app/src/main/java/kz/jusan/singularityhomeworks/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity(), ItemTouchDelegate {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         initRecyclerView()
         populateWithData()
