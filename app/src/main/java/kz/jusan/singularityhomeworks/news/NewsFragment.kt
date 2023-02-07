96c6945efc32e415ef8e580f2872434c3bab212b
Ulpan Beken <u.beken@typi.team>
2023-02-07 19:29:57 +0600

    added close button (not working)

--- app/src/main/java/kz/jusan/singularityhomeworks/news/NewsFragment.kt
+++ app/src/main/java/kz/jusan/singularityhomeworks/news/NewsFragment.kt
@@ -2,9 +2,12 @@ package kz.jusan.singularityhomeworks.news
 
 import android.app.ProgressDialog.show
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -15,14 +18,12 @@ import kz.jusan.singularityhomeworks.R
 import kotlin.math.abs
 
 
-class FavoritesFragment : Fragment(), MotionLayout.TransitionListener {
+class FavoritesFragment : Fragment() {
 
     private lateinit var rvNews : RecyclerView
     private lateinit var newsAdapter : NewsAdapter
 
-    private var lastProgress = 0f
     private var fragment: Fragment? = null
-    private var last: Float = 0f
 
 
     override fun onCreateView(
@@ -47,8 +48,10 @@ class FavoritesFragment : Fragment(), MotionLayout.TransitionListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<MotionLayout>(R.id.motionLayout).setTransitionListener(this)
-
+        view.findViewById<ImageView>(R.id.btn_close).setOnClickListener {
+            view.findViewById<FrameLayout>(R.id.fragment_container).visibility = View.GONE
+            Log.e("NewsFragment", "button clicked")
+        }
         initRecyclerView(view)
         loadData()
     }
@@ -72,55 +75,4 @@ class FavoritesFragment : Fragment(), MotionLayout.TransitionListener {
 
         newsAdapter.updateDataSet(news)
     }
-
-    override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
-    }
-
-    override fun onTransitionChange(
-        p0: MotionLayout?, p1: Int, p2: Int, p3: Float
-    ) {
-        if (p3 - lastProgress > 0) {
-            // from start to end
-            val atEnd = abs(p3 - 1f) < 0.1f
-            if (atEnd && fragment is MinPlayerFragment) {
-                val transaction = requireFragmentManager().beginTransaction()
-                transaction
-                    .setCustomAnimations(R.animator.show, 0)
-                fragment = MaxPlayerFragment.newInstance().also {
-                    transaction
-                        .setCustomAnimations(R.animator.show, 0)
-                        .replace(R.id.fragment_container, it)
-                        .commitNow()
-                }
-            }
-        } else {
-            // from end to start
-            val atStart = p3 < 0.9f
-            if (atStart && fragment is MaxPlayerFragment) {
-                val transaction = requireFragmentManager().beginTransaction()
-                transaction
-                    .setCustomAnimations(0, R.animator.hide)
-                fragment = MinPlayerFragment.newInstance().also {
-                    transaction
-                        .replace(R.id.fragment_container, it)
-                        .commitNow()
-                }
-            }
-        }
-        lastProgress = p3
-    }
-
-    override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-
-    }
-
-    override fun onTransitionTrigger(
-        motionLayout: MotionLayout?,
-        triggerId: Int,
-        positive: Boolean,
-        progress: Float
-    ) {
-
-    }
-
 }
